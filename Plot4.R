4d85e28c0ab8d5333d22c9554b8f542425d57c2a
Erin Boyle <erinboyle05@gmail.com>
2016-02-19 18:27:53 -0500

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -2,5 +2,6 @@ require(dplyr)
 require(tidyr)
 coal <- SCC %>% filter(grepl('Combustion', SCC.Level.One)) %>%
         filter(grepl('Coal', SCC.Level.Three))
-plot4data <- NEI %>% filter(grepl(coal, SCC)) %>% group_by(year) %>%
-        summarise(Emissions = sum(Emissions))
+coal <- coallist$SCC
+#plot4data <- NEI %>% filter(grepl(coal, SCC)) %>% group_by(year) %>%
+#        summarise(Emissions = sum(Emissions))
