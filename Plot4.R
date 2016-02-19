3165023bf32786557102557658cc902970cca746
Erin Boyle <erinboyle05@gmail.com>
2016-02-19 18:24:16 -0500

    One more script

---
+++ Plot4.R
@@ -0,0 +1,6 @@
+require(dplyr)
+require(tidyr)
+coal <- SCC %>% filter(grepl('Combustion', SCC.Level.One)) %>%
+        filter(grepl('Coal', SCC.Level.Three))
+plot4data <- NEI %>% filter(grepl(coal, SCC)) %>% group_by(year) %>%
+        summarise(Emissions = sum(Emissions))
